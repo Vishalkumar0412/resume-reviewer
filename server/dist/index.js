46e11a869a80254e0d7a20ec400907a4345a7067
vishal kumar <iamvishalnigam@gmail.com>
2025-08-02 23:01:35 +0530

    packgeJson

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,24 +5,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const routes_1 = __importDefault(require("./routes"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const routes_1 = __importDefault(require("./routes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
+// Middlewares
 app.use(express_1.default.json());
 app.use(express_1.default.text());
 app.use((0, cookie_parser_1.default)());
-console.log(process.env.CLIENT_URL);
-app.use((0, cors_1.default)({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
+// CORS setup
+const allowedOrigin = process.env.CLIENT_URL || '*';
+console.log('Allowed Origin:', allowedOrigin);
+app.use((0, cors_1.default)());
+// app.use(cors({
+//   origin: allowedOrigin,
+//   credentials: true,
+// }));
+// Routes
 app.use('/api/v1', routes_1.default);
-app.get('test', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Server is running');
 });
+// Start server
 app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
