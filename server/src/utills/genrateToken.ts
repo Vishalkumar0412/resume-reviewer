999f0db19cb62c4466dc11f6f9528fbb10618bd4
vishal kumar <iamvishalnigam@gmail.com>
2025-08-03 09:52:51 +0530

    prod-1

--- server/src/utills/genrateToken.ts
+++ server/src/utills/genrateToken.ts
@@ -1,20 +1,25 @@
 import jwt from "jsonwebtoken";
 import { Response } from "express";
-export const genrateToken = async (
+
+export const generateToken = async (
   res: Response,
   user: any,
   message: string
 ) => {
-  const token = jwt.sign(user, process.env.JWT_SECRET!);
+  const token = jwt.sign(user, process.env.JWT_SECRET!, {
+    expiresIn: "7d", // optional but good practice
+  });
+
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
+
   return res.status(200).json({
     success: true,
     message,
     token,
-  
   });
-};
+}
